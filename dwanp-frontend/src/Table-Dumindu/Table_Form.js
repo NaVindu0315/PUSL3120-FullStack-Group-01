86b1358460b296a19be32f4378240898c46e441f
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 16:30:30 +0530

    sad

---
+++ dwanp-frontend/src/Table-Dumindu/Table_Form.js
@@ -0,0 +1,15 @@
+
+///////////////////////////
+//Dumind
+//////////////////////
+
+
+import React, { useEffect, useState } from "react";
+import { Button, Grid, Typography, Input } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+
+const TableForm = ({props}) =>
+{
+
+}
+export default TableForm;
