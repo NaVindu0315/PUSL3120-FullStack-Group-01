7d2fc67bff75f4543d640565e24cad34cee402df
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 20:16:33 +0530

    sada

--- dwanp-frontend/src/Employee-Navindu/Employee.js
+++ dwanp-frontend/src/Employee-Navindu/Employee.js
@@ -5,6 +5,7 @@
 //importing
 import { Box } from "@mui/material";
 import {  Typography, Container} from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
 //import for connecting backend and front end
 //import Axios from "axios";
@@ -12,9 +13,14 @@ import {  Typography, Container} from "@mui/material";
 
 const Employee=()=>
 {
+  const navigate = useNavigate();
     return (
         <Container maxWidth="xl">
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
+            <Typography variant="h5" component="h5"> 
+            <button className="usr-btn" onClick={()=>navigate('/')}>Back</button>
+            
+            </Typography>
             <Typography variant="h2" component="h1">
               Employee Page
             </Typography>
