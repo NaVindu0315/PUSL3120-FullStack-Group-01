e451d1cd5dde8c7590bcfefabeb149c10a5cc20d
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 20:28:34 +0530

    asda

--- dwanp-frontend/src/Order-wish/Order.js
+++ dwanp-frontend/src/Order-wish/Order.js
@@ -17,6 +17,10 @@ const Order=()=>
     return (
         <Container maxWidth="xl">
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
+          <Typography variant="h5" component="h5"> 
+            <button className="usr-btn" onClick={()=>navigate('/')}>Back</button>
+            
+            </Typography>
             <Typography variant="h2" component="h1">
               Order Page
             </Typography>
