8888c2612901b15764d758473841fb013d308d77
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 20:17:31 +0530

    sasa

--- dwanp-frontend/src/Inventory-pasindu/Inventory.js
+++ dwanp-frontend/src/Inventory-pasindu/Inventory.js
@@ -5,6 +5,7 @@
 //importing
 import { Box } from "@mui/material";
 import {  Typography, Container} from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
 //import for connecting backend and front end
 //import Axios from "axios";
