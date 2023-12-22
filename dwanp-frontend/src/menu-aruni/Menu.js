2d4d30d7427e2f3b15e97a00ea2f4e2a49f7144c
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 20:26:18 +0530

    asd

--- dwanp-frontend/src/menu-aruni/Menu.js
+++ dwanp-frontend/src/menu-aruni/Menu.js
@@ -14,6 +14,7 @@ import {  Typography, Container} from "@mui/material";
 
 const Menu2=()=>
 {
+  const navigate = useNavigate();
     return (
         <Container maxWidth="xl">
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
