6908a5e3c442aa66103eebeb404d2ce8ee52c5aa
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 22:28:34 +0530

    sda

--- dwanp-backend/models/menu_model.js
+++ dwanp-backend/models/menu_model.js
@@ -1,1 +1,19 @@
 //aruni
+//meke employeeschema wenuwt enna one ubala krana eka ex: orderSchema ,tableSchema
+//const Employee wenuwt ubalage ex: Order,Tablereserver
+//cll ekk dpn meka ghnna kalin
+
+/*
+const mongoose = require('mongoose');
+const Schema  = mongoose.Schema;
+
+///defining employee schema
+const employeeSchema = new Schema(
+    {
+        id:Number,
+        name : String,
+    }
+);
+const Employee = mongoose.model('employees',employeeSchema);
+module.exports = Employee;
+*/
