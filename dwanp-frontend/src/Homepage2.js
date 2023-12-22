1758d00b506190f25a08b0af3cce18f15391cd6e
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 15:29:43 +0530

    table

--- dwanp-frontend/src/Homepage2.js
+++ dwanp-frontend/src/Homepage2.js
@@ -1,23 +1,53 @@
 //this is the homepage
 
-import {Button,Container,Box,Typography,Grid} from "@mui/material";
+import { Button, Container, Box, Typography, Grid, Table, TableCell, TableHead,TableRow,TableBody } from "@mui/material";
 
-const Homepage  = ({props}) =>
-{
-    return(
+const Homepage = ({ props }) => {
+    return (
         <Container maxWidth="xl">
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
-      <Typography variant="h2" component="h1">
-        Homepage
-      </Typography>
-      <Typography variant="h5" sx={{ mt: 2 }}>
-        Welcome
-      </Typography>
-    </Box>
-    <Grid container spacing={4}>
-      Welcome
-    </Grid>
-  </Container>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
+                <Typography variant="h2" component="h1">
+                    Homepage
+                </Typography>
+                <Typography variant="h5" sx={{ mt: 2 }}>
+                    Welcome
+                </Typography>
+            </Box>
+            <Grid container spacing={4}>
+                <Table sx={{ mt: 4 }}>
+                    <TableHead>
+
+
+                        <TableRow>
+
+
+                            <TableCell>Column 1</TableCell>
+
+
+                            <TableCell>Column 2</TableCell>
+
+
+                            <TableCell>Column 3</TableCell>
+
+
+                        </TableRow>
+
+
+                    </TableHead>
+
+
+                    <TableBody>
+                  {/*first row */}
+                            <TableRow >
+                                <TableCell>1</TableCell>
+                                <TableCell>2</TableCell>
+                                <TableCell>3</TableCell>
+                            </TableRow>
+                        
+                    </TableBody>
+                </Table>
+            </Grid>
+        </Container>
 
     )
 
