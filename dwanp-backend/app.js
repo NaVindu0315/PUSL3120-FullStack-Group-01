95efce8437659c51243a8ce198709c6a18f945b4
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 22:25:50 +0530

    sdsa

--- dwanp-backend/app.js
+++ dwanp-backend/app.js
@@ -10,10 +10,68 @@ const cors  = require('cors');
 app.use(cors());
 
 ///for cruds for each 
-//meke ubala nama tyna thanat adala his ide ghpn wena ewage ghanna epa functions
+//meke ubala nama tyna thanat adala his ide ghpn wena ewage ghanna epa functions  comment eke tyna nama use krpn function ekatath
+ 
+//employee - navindu
+///getemployee
 
 
+///createemployee
 
+
+///updateemployee
+
+
+///delete employee
+
+
+///////////////////////////////
+//menu - aruni
+
+//getmenu
+
+//createmenu
+
+//deletemenu
+
+//updatemenu
+
+/////////////////////
+//inventory - pasindu
+
+//getinventory
+
+//createinventory
+
+//deleteinventory
+
+//updateinventory
+
+/////////////////////
+//order wish
+
+//getoder
+
+//createorder
+
+//updateorder
+
+//deleteorder
+
+////////////////////
+
+//table - dumindu
+
+//gettableview
+
+
+//addtable
+
+
+//deletetable
+
+
+//deletetable
 
 ///
 module.exports = app;
