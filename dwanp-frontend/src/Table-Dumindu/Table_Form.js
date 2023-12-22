fe6ab2c99ee254a68caadb9c767f54ca8301baa2
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 20:31:50 +0530

    asa

--- dwanp-frontend/src/Table-Dumindu/Table_Form.js
+++ dwanp-frontend/src/Table-Dumindu/Table_Form.js
@@ -5,7 +5,7 @@
 
 
 import React, { useEffect, useState } from "react";
-import { Button, Grid, Typography, Input } from "@mui/material";
+//import { Button, Grid, Typography, Input } from "@mui/material";
 //import { useNavigate } from 'react-router-dom';
 
 const TableForm = ({props}) =>
