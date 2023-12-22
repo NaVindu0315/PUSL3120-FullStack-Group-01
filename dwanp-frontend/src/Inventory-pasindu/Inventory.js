4e3a665c8db0a2418a77789a36c9b074ce0deeff
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 20:24:40 +0530

    ads

--- dwanp-frontend/src/Inventory-pasindu/Inventory.js
+++ dwanp-frontend/src/Inventory-pasindu/Inventory.js
@@ -13,9 +13,14 @@ import { useNavigate } from 'react-router-dom';
 
 const Inventory=()=>
 {
+  const navigate = useNavigate();
     return (
         <Container maxWidth="xl">
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
+          <Typography variant="h5" component="h5"> 
+            <button className="usr-btn" onClick={()=>navigate('/')}>Back</button>
+            
+            </Typography>
             <Typography variant="h2" component="h1">
               Inventory page
             </Typography>
