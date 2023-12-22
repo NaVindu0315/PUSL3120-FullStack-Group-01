bf48fadd2d5282182471cef2b1c9e0b505aaac4f
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 15:50:06 +0530

    sd

---
+++ dwanp-frontend/src/Inventory-pasindu/Inventory.js
@@ -0,0 +1,31 @@
+
+///////////////////////////
+//pasindu
+//////////////////////
+//importing
+import { Box } from "@mui/material";
+import Userform from "./userform";
+import UsersTable from "./UsersTable";
+//import for connecting backend and front end
+import Axios from "axios";
+import { useEffect ,useState } from "react";
+
+const Menu=()=>
+{
+    return (
+        <Container maxWidth="xl">
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
+            <Typography variant="h2" component="h1">
+              Inventory page
+            </Typography>
+            <Typography variant="h5" sx={{ mt: 2 }}>
+              Pasindu
+            </Typography>
+          </Box>
+          
+        </Container>
+      );
+      
+}
+
+export default Menu;
