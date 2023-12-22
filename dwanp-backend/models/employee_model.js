caef1f335108a629762e229741a4f06cf58f5c4b
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 22:16:19 +0530

    as

--- dwanp-backend/models/employee_model.js
+++ dwanp-backend/models/employee_model.js
@@ -3,3 +3,9 @@ const mongoose = require('mongoose');
 const Schema  = mongoose.Schema;
 
 ///defining employee schema
+const employeeSchema = new Schema(
+    {
+        id:Number,
+        name : String,
+    }
+);
