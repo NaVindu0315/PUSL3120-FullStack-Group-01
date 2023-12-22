96e54d2a98dd42ab1f027585e9c56bff6fa2d1fc
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 21:51:10 +0530

    as

--- dwanp-backend/Server.js
+++ dwanp-backend/Server.js
@@ -39,3 +39,8 @@ const connect = async()=>{
 }
 
 connect();
+const server = app.listen(port,host,()=>{
+    console.log('Node server is listenging to ',server.address().port);
+});
+
+app.use('/api',router);
