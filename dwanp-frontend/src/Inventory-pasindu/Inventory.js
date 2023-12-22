05fdec8daf9d573b7edff94d38b267ef2e91fa00
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 16:24:51 +0530

    asd

--- dwanp-frontend/src/Inventory-pasindu/Inventory.js
+++ dwanp-frontend/src/Inventory-pasindu/Inventory.js
@@ -4,8 +4,7 @@
 //////////////////////
 //importing
 import { Box } from "@mui/material";
-import Userform from "./userform";
-import UsersTable from "./UsersTable";
+
 //import for connecting backend and front end
 import Axios from "axios";
 import { useEffect ,useState } from "react";
