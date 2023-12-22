3fb664b25a465a429f7a3f3111b787670e49e2d1
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 15:37:33 +0530

    menu

--- dwanp-frontend/src/menu-aruni/Menu.js
+++ dwanp-frontend/src/menu-aruni/Menu.js
@@ -9,3 +9,21 @@ import UsersTable from "./UsersTable";
 //import for connecting backend and front end
 import Axios from "axios";
 import { useEffect ,useState } from "react";
+
+const Menu=()=>
+{
+    return (
+        <Container maxWidth="xl">
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
+            <Typography variant="h2" component="h1">
+              Homepage
+            </Typography>
+            <Typography variant="h5" sx={{ mt: 2 }}>
+              Menu Page
+            </Typography>
+          </Box>
+          
+        </Container>
+      );
+      
+}
