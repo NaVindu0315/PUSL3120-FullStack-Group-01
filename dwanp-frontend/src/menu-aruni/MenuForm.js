c2c5a5bd5b8ec7adcca0d62492090501a210743b
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 15:40:27 +0530

    s

--- dwanp-frontend/src/menu-aruni/MenuForm.js
+++ dwanp-frontend/src/menu-aruni/MenuForm.js
@@ -1,6 +1,15 @@
 
+///////////////////////////
+//Aruni
+//////////////////////
 
 
 import React, { useEffect, useState } from "react";
 import { Button, Grid, Typography, Input } from "@mui/material";
 import { useNavigate } from 'react-router-dom';
+
+const MenuForm = ({props}) =>
+{
+
+}
+export default MenuForm;
