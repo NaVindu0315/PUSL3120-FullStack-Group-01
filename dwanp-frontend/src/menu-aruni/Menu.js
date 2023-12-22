656e06faf74cc4410b3aa7df1c4e3a8e73af7b36
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-22 15:36:11 +0530

    aruni page

---
+++ dwanp-frontend/src/menu-aruni/Menu.js
@@ -0,0 +1,11 @@
+
+///////////////////////////
+//Aruni
+//////////////////////
+//importing
+import { Box } from "@mui/material";
+import Userform from "./userform";
+import UsersTable from "./UsersTable";
+//import for connecting backend and front end
+import Axios from "axios";
+import { useEffect ,useState } from "react";
