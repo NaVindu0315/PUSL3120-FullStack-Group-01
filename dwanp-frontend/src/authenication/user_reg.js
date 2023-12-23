5c9b2cfdcd123a444b63b5e1d922f436717444f9
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-23 19:27:19 +0530

    useregistration

---
+++ dwanp-frontend/src/authenication/user_reg.js
@@ -0,0 +1,81 @@
+import {  Container, Box, Typography, Grid, Table, TableCell, TableHead,TableRow,TableBody } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+
+const UserRegistration = ({props}) =>
+{
+    const navigate = useNavigate();
+    return(
+
+        <Container maxWidth="xl">
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
+            <Typography variant="h2" component="h1">
+                Homepage
+            </Typography>
+            <Typography variant="h5" sx={{ mt: 2 }}>
+                Welcome
+            </Typography>
+        </Box>
+        <Grid container spacing={4}>
+            <Table sx={{ mt: 4 }}>
+                <TableHead>
+
+
+                    <TableRow>
+
+
+                        <TableCell></TableCell>
+
+
+                        <TableCell></TableCell>
+
+
+                        <TableCell></TableCell>
+
+
+                    </TableRow>
+
+
+                </TableHead>
+
+
+                <TableBody>
+              {/*first row */}
+                        <TableRow >
+                            <TableCell></TableCell>
+                            <TableCell>
+                            <button className="usr-btn" onClick={()=>navigate('/menu')}>Menu</button>
+                            </TableCell>
+                            <TableCell>
+                            <button className="usr-btn" onClick={()=>navigate('/inventory')}>Inventory</button>
+                            </TableCell>
+                        </TableRow>
+            {/*second row */}
+                         <TableRow >
+                            <TableCell></TableCell>
+                            <TableCell>
+                            <button className="usr-btn" onClick={()=>navigate('/order')}>Order</button>
+                            </TableCell>
+                            <TableCell>
+                            <button className="usr-btn" onClick={()=>navigate('/employee')}>Employee</button>
+                            </TableCell>
+                        </TableRow>
+             {/*third row */}
+             <TableRow >
+                            <TableCell></TableCell>
+                            <TableCell>
+                            <button className="usr-btn" onClick={()=>navigate('/tableview')}>Table</button>
+                            </TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    
+                </TableBody>
+            </Table>
+        </Grid>
+    </Container>
+
+        
+
+
+    )
+}
+export default UserRegistration;
