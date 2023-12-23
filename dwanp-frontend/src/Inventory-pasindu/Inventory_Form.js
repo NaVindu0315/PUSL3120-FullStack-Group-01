93900157ccac05a2e0c519ff9555fcf350ab7f2f
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2023-12-23 11:26:20 +0530

    pasindu added

--- dwanp-frontend/src/Inventory-pasindu/Inventory_Form.js
+++ dwanp-frontend/src/Inventory-pasindu/Inventory_Form.js
@@ -1,15 +1,11 @@
-
 ///////////////////////////
 //Pasindu
 //////////////////////
 
-
+//Ratbathilake
 //import React, { useEffect, useState } from "react";
 //import { Button, Grid, Typography, Input } from "@mui/material";
 //import { useNavigate } from 'react-router-dom';
 
-const InventoryForm = ({props}) =>
-{
-
-}
+const InventoryForm = ({ props }) => {};
 export default InventoryForm;
