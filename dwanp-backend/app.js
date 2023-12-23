ceb0b6825f04fc55ddcdaa54c8e8057b27ee14cd
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2023-12-23 22:43:32 +0530

    before editing

--- dwanp-backend/app.js
+++ dwanp-backend/app.js
@@ -1,51 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors  = require('cors');
+const cors = require("cors");
 //for controllers
-const empcontroller = require('./controllers/employee_controller');
-
-
-
-
-
+const empcontroller = require("./controllers/employee_controller");
 
 app.use(cors());
 app.use(
-    express.urlencoded({
-        extended:true,
-    })
-    
-
-    
+  express.urlencoded({
+    extended: true,
+  })
 );
 app.use(express.json());
 
-///for cruds for each 
+///for cruds for each
 //meke ubala nama tyna thanat adala his ide ghpn wena ewage ghanna epa functions  comment eke tyna nama use krpn function ekatath
- 
+
 //employee - navindu
 ///getemployee
 
-
 ///createemployee
-app.post('/createemp',(req,res)=>
-{
-    empcontroller.addEmp(req.body,(callback)=>
-        {
-            res.send();
-
-
-        }
-    );
-
+app.post("/createemp", (req, res) => {
+  empcontroller.addEmp(req.body, (callback) => {
+    res.send();
+  });
 });
-
 
 ///updateemployee
 
-
 ///delete employee
-
 
 ///////////////////////////////
 //menu - aruni
@@ -86,12 +68,9 @@ app.post('/createemp',(req,res)=>
 
 //gettableview
 
-
 //addtable
 
-
 //deletetable
-
 
 //deletetable
 
