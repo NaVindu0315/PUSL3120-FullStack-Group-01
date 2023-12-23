8f058b12152d0f8def8eecf4d9bf6d022c0de0c6
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2023-12-23 21:06:20 +0530

    model and router create

--- dwanp-backend/models/order_model.js
+++ dwanp-backend/models/order_model.js
@@ -3,17 +3,16 @@
 //const Employee wenuwt ubalage ex: Order,Tablereserver
 //cll ekk dpn meka ghnna kalin
 
-/*
+
 const mongoose = require('mongoose');
 const Schema  = mongoose.Schema;
 
 ///defining employee schema
-const employeeSchema = new Schema(
+const orderSchema = new Schema(
     {
         id:Number,
         name : String,
     }
 );
-const Employee = mongoose.model('employees',employeeSchema);
-module.exports = Employee;
-*/
+const Order = mongoose.model('Order',orderSchema);
+module.exports = Order;
--- dwanp-backend/routes/order_router.js
+++ dwanp-backend/routes/order_router.js
@@ -1,1 +1,11 @@
 //wish
+const express = require('express');
+const router = express.Router();
+const controller = require('./controller');
+
+router.get('/Order', controller.getOrder);
+router.post('/createOrder', controller.addOrder);
+router.post('/updateOrder', controller.updateOrder);
+router.post('/deleteOrder', controller.deleteOrder);
+
+module.exports = router;
