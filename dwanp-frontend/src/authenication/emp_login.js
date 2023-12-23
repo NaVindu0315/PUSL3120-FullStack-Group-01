e4e582f2a5c8813c490f3351e37428b534dc5bc3
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-23 19:56:09 +0530

    emp login

---
+++ dwanp-frontend/src/authenication/emp_login.js
@@ -0,0 +1,90 @@
+import {  Container, Box, Typography, Grid, Table, TableCell, TableHead,TableRow,TableBody } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+import Employee from "../Employee-Navindu/Employee";
+
+const EmpLogin = ({props}) =>
+{
+    const navigate = useNavigate();
+    return(
+
+        <Container maxWidth="xl">
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
+            <Typography variant="h2" component="h1">
+                Employee Login
+            </Typography>
+            <Typography variant="h5" sx={{ mt: 2 }}>
+              Login
+            </Typography>
+        </Box>
+        <Grid container spacing={4}>
+            <Table sx={{ mt: 4 }}>
+                <TableHead>
+
+
+                    <TableRow>
+
+
+                        <TableCell></TableCell>
+
+
+                        <TableCell></TableCell>
+
+
+                        <TableCell></TableCell>
+
+
+                    </TableRow>
+
+
+                </TableHead>
+
+
+                <TableBody>
+              {/*first row */}
+                        <TableRow >
+                            <TableCell></TableCell>
+                            <TableCell>
+                           Nic
+                            </TableCell>
+                            <TableCell>
+                            <input
+                        type="text"
+                        //value={textValue}
+                            //onChange={(event) => setTextValue(event.target.value)}
+    />
+                            </TableCell>
+                        </TableRow>
+            {/*second row */}
+                         <TableRow >
+                            <TableCell></TableCell>
+                            <TableCell>
+                      Password
+                            </TableCell>
+                            <TableCell>
+                            <input
+                        type="text"
+                        //value={textValue}
+                            //onChange={(event) => setTextValue(event.target.value)}
+    />
+                            </TableCell>
+                        </TableRow>
+             {/*third row */}
+             <TableRow >
+                            <TableCell></TableCell>
+                            <TableCell>
+                          
+                            </TableCell>
+                            <TableCell> <button className="usr-btn" onClick={()=>navigate('/tableview')}>Login</button></TableCell>
+                        </TableRow>
+                    
+                </TableBody>
+            </Table>
+        </Grid>
+    </Container>
+
+        
+
+
+    )
+}
+export default EmpLogin;
