8027f7e945731596558aac0616818f262e3706e4
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2023-12-23 20:29:42 +0530

    Order and OrderForm pages

--- dwanp-frontend/src/Order-wish/Order.js
+++ dwanp-frontend/src/Order-wish/Order.js
@@ -4,34 +4,121 @@
 //////////////////////
 //importing
 import { Box } from "@mui/material";
+import OrderForm from "./OrderForm";
+import OrderTable from "./OrderTable";
 import { useNavigate } from 'react-router-dom';
+import Axios from "axios";
+import { useEffect, useState } from "react";
+import { Typography, Container } from "@mui/material";
 
-//import for connecting backend and front end
-//import Axios from "axios";
-//import { useEffect ,useState } from "react";
-import {  Typography, Container} from "@mui/material";
+const Order = () => {
+  const [Order, setOrder] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState({});
+  const [isEdit, setIsEdit] = useState(false);
 
-const Order=()=>
-{
-  const navigate = useNavigate();
-    return (
-        <Container maxWidth="xl">
-          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
-          <Typography variant="h5" component="h5"> 
-            <button className="usr-btn" onClick={()=>navigate('/')}>Back</button>
-            
-            </Typography>
-            <Typography variant="h2" component="h1">
-              Order Page
-            </Typography>
-            <Typography variant="h5" sx={{ mt: 2 }}>
-              Wish
-            </Typography>
-          </Box>
-          
-        </Container>
-      );
-      
+  useEffect(() => {
+    getOrder();
+  }, []);
+
+  const getOrder = () => {
+    Axios.get('http://localhost:3001/api/Order')
+    .then(response => {
+        setOrder(response.data.response || []);
+    })
+    .catch(error => {
+        console.error("Axios Error : ", error);
+    });
 }
+
+const addOrder = (data) => {
+  setSubmitted(true);
+
+  const payload ={
+      id:  data.id,
+      name: data.name,
+  }
+  Axios.post('http://localhost:3001/api/createOrder', payload)
+  .then(() => {
+      getOrder();
+      setSubmitted(false);
+      isEdit(false);
+  })
+  .catch(error => {
+      console.error("Axios Error : ", error);
+  });
+}
+
+const updateOrder = (data) => {
+setSubmitted(true);
+
+const payload ={
+    id:  data.id,
+    name: data.name,
+}
+Axios.post('http://localhost:3001/api/updateOrder', payload)
+.then(() => {
+    getOrder();
+    setSubmitted(false);
+    isEdit(false);
+})
+.catch(error => {
+    console.error("Axios Error : ", error);
+});
+}
+
+const deleteOrder = (data) => {
+Axios.post('http://localhost:3001/api/deleteOrder', data)
+.then(() => {
+    getOrder();
+})
+.catch(error => {
+    console.error("Axios Error : ", error);
+});
+}
+
+  const navigate = useNavigate();
+
+  return (
+    <> {/* Wrap multiple elements in a fragment */}
+      <Container maxWidth="xl">
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
+          <Typography variant="h5" component="h5">
+            <button className="usr-btn" onClick={() => navigate('/')}>Back</button>
+          </Typography>
+          <Typography variant="h2" component="h1">
+            Order Page
+          </Typography>
+          <Typography variant="h5" sx={{ mt: 2 }}>
+            Wish
+          </Typography>
+        </Box>
+      </Container>
+      <Box
+        sx={{
+          width: 'calc(100%-100px)',
+          margin: 'auto',
+          marginTop: '100px'
+        }}
+      >
+        <OrderForm
+          addOrder={addOrder}
+          updateOrder={updateOrder}
+          submitted={submitted}
+          data={selectedOrder}
+          isEdit={isEdit}
+        />
+        <OrderTable 
+            rows={Order}
+            selectedUser={data => {
+                setSelectedOrder(data);
+                setIsEdit(true);
+        }}
+        deleteUser ={data => window.confirm('Are you sure?') && deleteOrder(data)}
+        />
+      </Box>
+    </>
+  );
+};
 
 export default Order;
---
+++ dwanp-frontend/src/Order-wish/OrderForm.js
@@ -0,0 +1,125 @@
+
+///////////////////////////
+//Wish
+//////////////////////
+
+import React, { useEffect, useState } from "react";
+import { Button, Grid, Typography, Input } from "@mui/material";
+//import { useNavigate } from 'react-router-dom';
+
+
+const Order_Form = ({ addOrder, updateOrder, submitted, data, isEdit }) =>{
+    const [id, setId] = useState(0);
+      const [name, setName] = useState('');
+      useEffect(() => {
+        if (!submitted) {
+            setId(0);
+            setName('');
+        }
+    }, [submitted]);
+  
+    useEffect(() => {
+        if(data?.id !==0 && data.id !==0) {
+            setId(data.id);
+            setName(data.name)
+        }
+    }, [data])
+  
+    return(
+        <Grid
+        container
+        spacing={2}
+        sx={{
+            backgroundColor: '#ffffff',
+            marginBottom: '30px',
+            display:'block',
+            marginLeft: '30px'
+        }}
+        >
+            <Grid item xs={12} sm={6} sx={{display:'flex'}}>
+                <Typography 
+                component={'h1'} 
+                sx={{color:'#000000', marginLeft: '15px'}}>Order Form
+                </Typography>
+            </Grid>
+  
+            <Grid>
+                <Typography 
+                component={'label'} 
+                htmlFor="id"
+                sx={{
+                    color:'#000000',
+                    marginRight:'20px',
+                    fontSize:'16px',
+                    width:'100px',
+                    display:'block',
+                    marginLeft: '30px'
+            }}
+                >
+                    ID
+                </Typography>
+                <Input 
+                type="number"
+                id="id"
+                name="id"
+                sx={{ width:'400px', marginLeft: '30px'}}
+                value={id}
+                onChange={e => setId(e.target.value)}
+                />
+            </Grid>
+  
+            <Grid item xs={12} sm={6} sx={{display:'flex'}}>
+                <Typography 
+                component={'h1'} 
+                sx={{color:'#000000', marginLeft: '15px'}}>Order Form
+                </Typography>
+            </Grid>
+  
+            <Grid>
+                <Typography 
+                component={'label'} 
+                htmlFor="id"
+                sx={{
+                    color:'#000000',
+                    marginRight:'20px',
+                    fontSize:'16px',
+                    width:'100px',
+                    display:'block',
+                    marginLeft: '30px'
+            }}
+                >
+                    Name
+                </Typography>
+                <Input 
+                type="text"
+                id="name"
+                name="name"
+                sx={{ width:'400px',  marginLeft: '30px'}}
+                value={name}
+                onChange={e => setName(e.target.value)}
+                />
+            </Grid>
+            <Button
+            sx={{
+                margin:'auto',
+                marginBottom: '20px',
+                backgroundColor: '#00c6e6',
+                color: '#000000',
+                marginLeft: '30px',
+                marginTop:'20px',
+                '&:hover': {
+                    opacity:'0.7',
+                    backgroundColor: '#00c6e6'
+                }
+            }}
+            onClick={() => isEdit ? updateOrder({ id, name }) : addOrder({ id, name })}
+            >
+                {
+                    isEdit ? 'Update' : 'Add'
+                }
+            </Button>
+        </Grid>
+    );
+    }
+
+    export default Order_Form;
