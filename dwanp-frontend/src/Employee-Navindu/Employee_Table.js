1c753d89a7a8945c634da45b7a211c3382696bb2
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-24 15:12:52 +0530

    added emp tble to emp page

--- dwanp-frontend/src/Employee-Navindu/Employee_Table.js
+++ dwanp-frontend/src/Employee-Navindu/Employee_Table.js
@@ -4,12 +4,35 @@
 //////////////////////
 
 
-//import React, { useEffect, useState } from "react";
+import { Paper, TableCell, TableContainer, TableHead, TableRow , TableBody, Table} from "@mui/material";
+import React, { useEffect, useState } from "react";
 //import { Button, Grid, Typography, Input } from "@mui/material";
-//import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const EmployeeTable = ({props}) =>
 {
-
+    return(
+        <TableContainer 
+                component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>
+                                    ID
+                                </TableCell>
+                                <TableCell>
+                                    Name
+                                </TableCell>
+                                <TableCell>
+                                    Actions
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        
+                     
+                    </Table>
+        
+        </TableContainer>
+            );
 }
 export default EmployeeTable;
