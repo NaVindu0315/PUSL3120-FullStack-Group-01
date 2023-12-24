11813bea90bc2d03437ab4e100aa853a3d297ccb
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2023-12-24 12:44:57 +0530

    create get Operation with getInventory function

--- dwanp-backend/controllers/inventory_controller.js
+++ dwanp-backend/controllers/inventory_controller.js
@@ -1,1 +1,12 @@
-//pasindu
+const inventory = require("./inventory_model");
+
+const getInventory = (req, res, next) => {
+  inventory
+    .find()
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
+};
