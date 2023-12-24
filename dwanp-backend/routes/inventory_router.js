d75a6d0729c7273ea3f32b90c92cdbf41259020f
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2023-12-24 15:42:35 +0530

    set  the url  path for add inventory for router.js

--- dwanp-backend/routes/inventory_router.js
+++ dwanp-backend/routes/inventory_router.js
@@ -5,5 +5,8 @@ const router = express.Router();
 const invcontroller = require("../controllers/inventory_controller");
 
 router.get("/inventory", invcontroller.getInventory);
+router.post("/createinventory", invcontroller.addInventory);
+router.get("/inventory", invcontroller.getInventory);
+router.get("/inventory", invcontroller.getInventory);
 
 module.exports = router;
