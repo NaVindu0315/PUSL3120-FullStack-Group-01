f8d2ba86ce9ab6fbe892ec07b450a4f5069e311d
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2023-12-24 15:43:49 +0530

    set the url path to delete inventory in router.js

--- dwanp-backend/routes/inventory_router.js
+++ dwanp-backend/routes/inventory_router.js
@@ -7,6 +7,6 @@ const invcontroller = require("../controllers/inventory_controller");
 router.get("/inventory", invcontroller.getInventory);
 router.post("/createinventory", invcontroller.addInventory);
 router.get("/inventory", invcontroller.getInventory);
-router.get("/inventory", invcontroller.getInventory);
+router.post("/deleteinventory", invcontroller.deleteInventory);
 
 module.exports = router;
