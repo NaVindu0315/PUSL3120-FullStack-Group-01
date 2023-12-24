de62676a27783bdd94a130c239834bb94d8ce87b
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2023-12-24 15:41:06 +0530

    set the url path of get inventory to router

--- dwanp-backend/routes/inventory_router.js
+++ dwanp-backend/routes/inventory_router.js
@@ -3,3 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const invcontroller = require("../controllers/inventory_controller");
+
+router.get("/inventory", invcontroller.getInventory);
+
+module.exports = router;
