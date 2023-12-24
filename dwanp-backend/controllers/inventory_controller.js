1b66bfa0218988607a92767edd66f497d3471f66
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2023-12-24 12:46:38 +0530

    create AddInventory function

--- dwanp-backend/controllers/inventory_controller.js
+++ dwanp-backend/controllers/inventory_controller.js
@@ -10,3 +10,18 @@ const getInventory = (req, res, next) => {
       res.json({ error });
     });
 };
+
+const addInventory = (req, res, next) => {
+  const user = new User({
+    id: req.body.id,
+    name: req.body.name,
+  });
+  user
+    .save()
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
+};
