f402c562ea054454db39d1ff985ad1d64dca038b
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-25 15:31:07 +0530

    removed inline and chgned to external

--- dwanp-frontend/src/Employee-Navindu/Employee.js
+++ dwanp-frontend/src/Employee-Navindu/Employee.js
@@ -12,7 +12,8 @@ import EmployeeTable from "./Employee_Table";
 //import for connecting backend and front end
 import Axios from "axios";
 import { useEffect ,useState } from "react";
-//import './empstyle.css';
+import './empstyle.css';
+
 
 const Employee=()=>
 {
@@ -155,7 +156,7 @@ const Employee=()=>
 
 
     return (
-        <Container maxWidth="xl">
+        <Container maxWidth="xl" >
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
             <Typography variant="h5" component="h5"> 
             <button className="usr-btn" onClick={()=>navigate('/')}>Back</button>
