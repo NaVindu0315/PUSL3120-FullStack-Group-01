0249b16f3edc5c17879e692abaa943fa4cd5c2f5
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2023-12-25 23:56:40 +0530

    create Invnt item in the form

--- dwanp-frontend/src/Inventory-pasindu/Inventory_Form.js
+++ dwanp-frontend/src/Inventory-pasindu/Inventory_Form.js
@@ -3,7 +3,7 @@
 //////////////////////
 
 import { useEffect, useState } from "react";
-import { Button, Grid, Typography, Input } from "@mui/material";
+import { Button, Grid, Typography, Input, TableContainer } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 const InventoryForm = ({ addInv, updateInv, submitted, data, isEdit }) => {
@@ -27,8 +27,11 @@ const InventoryForm = ({ addInv, updateInv, submitted, data, isEdit }) => {
 
   useEffect(() => {
     if (data?.id && data.id !== 0) {
-      setId(data.id);
-      setName(data.name);
+      setItem(data.invnt_item);
+      setCode(data.invnt_code);
+      setQnt(data.qnty);
+      setPrc(data.price);
+      setDate(data.date);
     }
   }, [data]);
 
@@ -36,85 +39,177 @@ const InventoryForm = ({ addInv, updateInv, submitted, data, isEdit }) => {
     <Grid
       container
       spacing={2}
-      sx={{
-        backgroundColor: "#ffffff",
-        marginBottom: "30px",
-        display: "block",
-      }}
+      sx={{ backgroundColor: "#ffffff", marginBottom: "30px" }}
     >
       <Grid item xs={12}>
-        <Typography component={"h1"} sx={{ color: "#000000" }}>
-          User Form
+        <Typography component="h1" sx={{ color: "#000000" }}>
+          Inventory
         </Typography>
       </Grid>
 
-      <Grid item xs={12} sm={6} sx={{ display: "flex" }}>
-        <Typography
-          component={"label"}
-          htmlFor="id"
-          sx={{
-            color: "#000000",
-            marginRight: "20px",
-            fontSize: "16px",
-            width: "100px",
-            display: "block",
-          }}
-        >
-          ID
-        </Typography>
-        <Input
-          type="number"
-          id="id"
-          name="id"
-          sx={{ width: "400px" }}
-          value={id}
-          onChange={(e) => setId(e.target.value)}
-        />
-      </Grid>
-
-      <Grid item xs={12} sm={6} sx={{ display: "flex" }}>
-        <Typography
-          component={"label"}
-          htmlFor="id"
-          sx={{
-            color: "#000000",
-            marginRight: "20px",
-            fontSize: "16px",
-            width: "100px",
-            display: "block",
-          }}
-        >
-          Name
-        </Typography>
-        <Input
-          type="text"
-          id="name"
-          name="name"
-          sx={{ width: "400px" }}
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </Grid>
-
-      <Button
-        sx={{
-          margin: "auto",
-          marginBottom: "20px",
-          backgroundColor: "#00c6e6",
-          color: "#000000",
-          marginLeft: "15px",
-          marginTop: "20px",
-          "&:hover": {
-            opacity: "0.7",
-            backgroundColor: "#00c6e6",
-          },
-        }}
-        onClick={() =>
-          isEdit ? updateInv({ id, name }) : addInv({ id, name })
-        }
-      >
-        {isEdit ? "Update" : "Add"}
-      </Button>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Inventory Item</TableCell>
+              <TableCell>
+                <Input
+                  type="text"
+                  id="invnt_item"
+                  name="invnt_item"
+                  sx={{ width: "400px" }}
+                  value={invnt_item}
+                  onChange={(e) => setItem(e.target.value)}
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Employee Name</TableCell>
+              <TableCell>
+                <Input
+                  type="text"
+                  id="emp_name"
+                  name="emp_name"
+                  sx={{ width: "400px" }}
+                  value={emp_name}
+                  onChange={(e) => setEmpName(e.target.value)}
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>BirthDay</TableCell>
+              <TableCell>
+                <Input
+                  type="text"
+                  id="bdate"
+                  name="bdate"
+                  sx={{ width: "400px" }}
+                  value={bdate}
+                  onChange={(e) => setBdate(e.target.value)}
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>NIC</TableCell>
+              <TableCell>
+                <Input
+                  type="number"
+                  id="nic"
+                  name="nic"
+                  sx={{ width: "400px" }}
+                  value={nic}
+                  onChange={(e) => setNic(e.target.value)}
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Address</TableCell>
+              <TableCell>
+                <Input
+                  type="text"
+                  id="adrs"
+                  name="adrs"
+                  sx={{ width: "400px" }}
+                  value={adrs}
+                  onChange={(e) => setAdrs(e.target.value)}
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Telephone Number</TableCell>
+              <TableCell>
+                <Input
+                  type="number"
+                  id="tel"
+                  name="tel"
+                  sx={{ width: "400px" }}
+                  value={tel}
+                  onChange={(e) => setTel(e.target.value)}
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Married Status</TableCell>
+              <TableCell>
+                <Input
+                  type="text"
+                  id="civil"
+                  name="civil"
+                  sx={{ width: "400px" }}
+                  value={civil}
+                  onChange={(e) => setCivil(e.target.value)}
+                />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Button
+                  sx={{
+                    margin: "auto",
+                    marginBottom: "20px",
+                    backgroundColor: "#00c6e6",
+                    color: "#000000",
+                    marginLeft: "15px",
+                    marginTop: "20px",
+                    "&:hover": {
+                      opacity: 0.7,
+                      backgroundColor: "#00c6e6",
+                    },
+                  }}
+                  onClick={() => {
+                    addEmp({
+                      emp_code,
+                      emp_name,
+                      bdate,
+                      nic,
+                      adrs,
+                      tel,
+                      civil,
+                    });
+                  }}
+                >
+                  Add
+                </Button>
+              </TableCell>
+              <TableCell>
+                <Button
+                  sx={{
+                    margin: "auto",
+                    marginBottom: "20px",
+                    backgroundColor: "#00c6e6",
+                    color: "#000000",
+                    marginLeft: "15px",
+                    marginTop: "20px",
+                    "&:hover": {
+                      opacity: 0.7,
+                      backgroundColor: "#00c6e6",
+                    },
+                  }}
+                  onClick={() => {
+                    updateEmp({
+                      emp_code,
+                      emp_name,
+                      bdate,
+                      nic,
+                      adrs,
+                      tel,
+                      civil,
+                    });
+                  }}
+                >
+                  Update
+                </Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Tab>
     </Grid>
   );
 };
