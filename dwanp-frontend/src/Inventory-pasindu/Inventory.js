5db9faa2cd8dc32322d6e0084c302982c30e732c
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2023-12-25 18:39:10 +0530

    import inventory form and inventory table to the Inventory.js

--- dwanp-frontend/src/Inventory-pasindu/Inventory.js
+++ dwanp-frontend/src/Inventory-pasindu/Inventory.js
@@ -1,37 +1,41 @@
-
 ///////////////////////////
 //pasindu
 //////////////////////
 //importing
 import { Box } from "@mui/material";
-import {  Typography, Container} from "@mui/material";
-import { useNavigate } from 'react-router-dom';
+import { Typography, Container } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import InventoryForm from "./Inventory_Form";
+import InventoryTable from "./Inventory_Table";
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
-//import for connecting backend and front end
-//import Axios from "axios";
-//import { useEffect ,useState } from "react";
-
-const Inventory=()=>
-{
+const Inventory = () => {
   const navigate = useNavigate();
-    return (
-        <Container maxWidth="xl">
-          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
-          <Typography variant="h5" component="h5"> 
-            <button className="usr-btn" onClick={()=>navigate('/')}>Back</button>
-            
-            </Typography>
-            <Typography variant="h2" component="h1">
-              Inventory page
-            </Typography>
-            <Typography variant="h5" sx={{ mt: 2 }}>
-              Pasindu
-            </Typography>
-          </Box>
-          
-        </Container>
-      );
-      
-}
+  return (
+    <Container maxWidth="xl">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          mt: 4,
+        }}
+      >
+        <Typography variant="h5" component="h5">
+          <button className="usr-btn" onClick={() => navigate("/")}>
+            Back
+          </button>
+        </Typography>
+        <Typography variant="h2" component="h1">
+          Inventory page
+        </Typography>
+        <Typography variant="h5" sx={{ mt: 2 }}>
+          Pasindu
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
 
 export default Inventory;
--- dwanp-frontend/src/Inventory-pasindu/Inventory_Table.js
+++ dwanp-frontend/src/Inventory-pasindu/Inventory_Table.js
@@ -1,15 +1,67 @@
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
-///////////////////////////
-//Pasindu
-//////////////////////
+const UsersTable = ({ rows, selectInv, deleteInv }) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.length > 0 ? (
+            rows.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell>
+                  <Button
+                    sx={{ margin: "0px 10px" }}
+                    onClick={() => selectInv({ id: row.id, name: row.name })}
+                  >
+                    Update
+                  </Button>
+                  <Button
+                    sx={{ margin: "0px 10px" }}
+                    onClick={() => deleteInv({ id: row.id })}
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                No Data
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
-
-//import React, { useEffect, useState } from "react";
-//import { Button, Grid, Typography, Input } from "@mui/material";
-//import { useNavigate } from 'react-router-dom';
-
-const InventoryTable = ({props}) =>
-{
-
-}
-export default InventoryTable;
+export default UsersTable;
