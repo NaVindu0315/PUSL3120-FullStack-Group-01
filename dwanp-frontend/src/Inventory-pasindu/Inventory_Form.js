e6c6fdb3989548b85241af313899819e599f2a90
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2023-12-25 18:26:45 +0530

    assign the id and name to updateInv and addInv

--- dwanp-frontend/src/Inventory-pasindu/Inventory_Form.js
+++ dwanp-frontend/src/Inventory-pasindu/Inventory_Form.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 import { Button, Grid, Typography, Input } from "@mui/material";
 //import { useNavigate } from 'react-router-dom';
 
-const InventoryForm = ({ submitted, data }) => {
+const InventoryForm = ({ addInv, updateInv, submitted, data, isEdit }) => {
   const [id, setId] = useState(0);
   const [name, setName] = useState("");
 
@@ -22,6 +22,92 @@ const InventoryForm = ({ submitted, data }) => {
       setId(data.id);
       setName(data.name);
     }
-  });
+  }, [data]);
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        backgroundColor: "#ffffff",
+        marginBottom: "30px",
+        display: "block",
+      }}
+    >
+      <Grid item xs={12}>
+        <Typography component={"h1"} sx={{ color: "#000000" }}>
+          User Form
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12} sm={6} sx={{ display: "flex" }}>
+        <Typography
+          component={"label"}
+          htmlFor="id"
+          sx={{
+            color: "#000000",
+            marginRight: "20px",
+            fontSize: "16px",
+            width: "100px",
+            display: "block",
+          }}
+        >
+          ID
+        </Typography>
+        <Input
+          type="number"
+          id="id"
+          name="id"
+          sx={{ width: "400px" }}
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+      </Grid>
+
+      <Grid item xs={12} sm={6} sx={{ display: "flex" }}>
+        <Typography
+          component={"label"}
+          htmlFor="id"
+          sx={{
+            color: "#000000",
+            marginRight: "20px",
+            fontSize: "16px",
+            width: "100px",
+            display: "block",
+          }}
+        >
+          Name
+        </Typography>
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          sx={{ width: "400px" }}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </Grid>
+
+      <Button
+        sx={{
+          margin: "auto",
+          marginBottom: "20px",
+          backgroundColor: "#00c6e6",
+          color: "#000000",
+          marginLeft: "15px",
+          marginTop: "20px",
+          "&:hover": {
+            opacity: "0.7",
+            backgroundColor: "#00c6e6",
+          },
+        }}
+        onClick={() =>
+          isEdit ? updateInv({ id, name }) : addInv({ id, name })
+        }
+      >
+        {isEdit ? "Update" : "Add"}
+      </Button>
+    </Grid>
+  );
 };
 export default InventoryForm;
