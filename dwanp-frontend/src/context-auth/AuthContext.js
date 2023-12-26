a26b755fe5126af88657108eae2ca2b7cbfa1f7f
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-26 20:25:52 +0530

    addd authredcuer to new authcontexyt

--- dwanp-frontend/src/context-auth/AuthContext.js
+++ dwanp-frontend/src/context-auth/AuthContext.js
@@ -1,3 +1,25 @@
 import {CreateContext,useReducer} from 'react'
 
 export const AuthContext = createContext()
+
+export const authReducer = (state,action) =>
+{
+    switch (action.type)
+    {
+        case 'LOGIN':
+            return {user : action .payload }
+        case 'LOGOUT':
+            return { user :null}
+        default :
+            return state
+    }
+}
+
+
+export const AuthContextProvider   = ({children}) =>
+{
+    const  [state,dispatch] = useReducer(authReducer,{
+        //might be signupuser as i used signupuser in the backend 
+        user:null
+    })
+}
