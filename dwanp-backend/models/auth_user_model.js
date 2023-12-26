0f9cdf8c1997806b8999eae4cea392ffa39edf16
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-26 10:46:51 +0530

    added email in userschema

---
+++ dwanp-backend/models/auth_user_model.js
@@ -0,0 +1,14 @@
+const mongoose = require ('mongoose')
+
+const Schema  = mongoose.Schema
+
+const authusersSchema  = new Schema (
+    {
+        email :
+        {
+            type :String,
+            required :true,
+            unique : true
+        }
+    }
+)
