2cf8c7c7b89eef0eb3bcbb50504b0cf5fcee4090
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-26 20:53:41 +0530

    add contexnt provider inside app.js

--- dwanp-frontend/src/App.js
+++ dwanp-frontend/src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
+import { AuthContextProvider } from './context-auth/AuthContext';
 //for navigation
 //import { useNavigate } from 'react-router-dom';
 //import Homepage from './Homepage2';
 import Home from './Home';
-
+//in the tutorial this is done in the index.js 
+//
 function App() {
   return (
+    <AuthContextProvider>
 <Home></Home>
+    </AuthContextProvider>
+
   )
 }
 
