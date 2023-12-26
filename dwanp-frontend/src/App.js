c177abcd645c592c3df988e0cadf4da00524e024
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-26 21:12:20 +0530

    useauthcontexthook

--- dwanp-frontend/src/App.js
+++ dwanp-frontend/src/App.js
@@ -6,7 +6,7 @@ import { AuthContextProvider } from './context-auth/AuthContext';
 //import Homepage from './Homepage2';
 import Home from './Home';
 //in the tutorial this is done in the index.js 
-//
+//in my index.js theres no wjhere to call this
 function App() {
   return (
     <AuthContextProvider>
