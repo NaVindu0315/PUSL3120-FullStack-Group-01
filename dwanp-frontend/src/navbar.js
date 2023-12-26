f720022a70eaf6349f97edd5b15f806bc81237cd
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2023-12-26 10:52:00 +0530

    import libraries

--- dwanp-frontend/src/navbar.js
+++ dwanp-frontend/src/navbar.js
@@ -2,3 +2,14 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { Container } from "@mui/material";
+
+<Container maxWidth="xl">
+  <AppBar position="static"> {/* Add this line */}
+    <Toolbar>
+      {/* Navbar content will go here */}
+    </Toolbar>
+  </AppBar>
+
+  ... rest of your code
+</Container>
