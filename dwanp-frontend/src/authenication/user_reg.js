a433ae6c977963121d1c673db10c30d972b54d0f
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-27 19:39:09 +0530

    Refactor user registration component

--- dwanp-frontend/src/authenication/user_reg.js
+++ dwanp-frontend/src/authenication/user_reg.js
@@ -1,10 +1,12 @@
 import { useState } from "react"
+import { AuthContext } from "../context-auth/AuthContext"
+import { AuthContextProvider } from "../context-auth/AuthContext"
 //import { useSignup } from "../hooks/useSignup"
 //import { useSignup } from "../hooks/useSignup"
 const SignUp = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
- // const {signup, error, isLoading} = useSignup()
+ //const {signup, error, isLoading} = useSignup()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -13,6 +15,7 @@ const SignUp = () => {
   }
 
   return (
+    <AuthContextProvider>
     <form className="signup" onSubmit={handleSubmit}>
       <h3>Sign Up</h3>
       
@@ -32,6 +35,7 @@ const SignUp = () => {
       <button >Sign up</button>
     
     </form>
+    </AuthContextProvider>
   )
 }
 
