e6d717c30c179d7742670d2773268ca0237939eb
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-27 18:27:33 +0530

    Fix signup API URL and update authcontext

--- dwanp-frontend/src/hooks/useSignup.js
+++ dwanp-frontend/src/hooks/useSignup.js
@@ -6,4 +6,35 @@ import { useauthcontext } from "./useAuthContext";
  {
     const [error,setError] = useState(null)
     const [isloading,setisloading] = useState(null)
+    const {dispatch} = useauthcontext()
+
+    const signup  = async (email,password) =>
+    {
+        setisloading(true)
+        setError(null)
+
+        //here he uses  ('api/user/signup')
+        //this is the backend url api/user/usersignup
+        //front end should be http://localhost:3000/userreg
+        const response = await fetch('api/user/usersignup',{
+            method : 'POST',
+            headers : {'Content-Type':'application/json'},
+            body : JSON.stringify({email,password})
+        })
+
+        const json =  await response.json()
+        if (!response.ok)
+        {
+            setisloading(false)
+            setError(json.error)
+        }
+        if(response.ok)
+        {
+            //saving the user to local storage 
+            localStorage,setItem('user',JSON.stringify(json))
+
+            //update the authcontext
+
+        }
+    }
  }
