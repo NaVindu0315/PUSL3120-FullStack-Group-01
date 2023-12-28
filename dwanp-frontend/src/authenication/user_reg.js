69b6130c9f1c8dc72a54b8c97d6cd79ae689f75f
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-28 18:25:56 +0530

    user registration works in front end

--- dwanp-frontend/src/authenication/user_reg.js
+++ dwanp-frontend/src/authenication/user_reg.js
@@ -1,40 +1,56 @@
-import { useState } from "react"
+import React, { useState } from "react";
+import Axios from "axios";
+import { Button, Table } from "@mui/material";
 
 const SignUp = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
- //const {signup, error, isLoading} = useSignup()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    console.log(email,password)
+    e.preventDefault();
 
-    //await signup(email, password)
-  }
+    try {
+      setSubmitted(true);
+      const response = await Axios.post('http://localhost:3001/api/user/usersignup', {
+        email,
+        password,
+      });
+
+      // Handle successful response, e.g., display a success message or redirect
+      console.log(response.data);
+      setSubmitted(false);
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.error("Axios Error:", error);
+      setSubmitted(false); // Reset loading state after error
+
+      // Handle error appropriately, e.g., display an error message to the user
+    }
+  };
 
   return (
-  
-    <form className="signup" onSubmit={handleSubmit}>
+    <Table>
       <h3>Sign Up</h3>
-      
+
       <label>Email address:</label>
-      <input 
-        type="email" 
-        onChange={(e) => setEmail(e.target.value)} 
-        value={email} 
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
       />
       <label>Password:</label>
-      <input 
-        type="password" 
-        onChange={(e) => setPassword(e.target.value)} 
-        value={password} 
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
       />
+      <Button disabled={submitted} onClick={handleSubmit}>
+        Sign Up
+      </Button>
+    </Table>
+  );
+};
 
-      <button >Sign up</button>
-    
-    </form>
-
-  )
-}
-
-export default SignUp
+export default SignUp;
