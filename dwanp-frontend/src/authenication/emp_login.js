03f3df56cfb30bc58a543ac82383aecbd9fe4142
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-29 00:48:27 +0530

    added employee login also

--- dwanp-frontend/src/authenication/emp_login.js
+++ dwanp-frontend/src/authenication/emp_login.js
@@ -1,94 +1,122 @@
-import {  Container, Box, Typography, Grid, Table, TableCell, TableHead,TableRow,TableBody } from "@mui/material";
+import { useState } from "react"
 import { useNavigate } from 'react-router-dom';
-import Employee from "../Employee-Navindu/Employee";
+import Axios from "axios";
+import { Button, Table ,Container, TableHead, TableRow, TableCell } from "@mui/material";
+
+//import { useNavigate } from 'react-router-dom';
+
+import Navbarr from "./Navbarr";
 
 const EmpLogin = ({props}) =>
 {
     const navigate = useNavigate();
-    return(
-
-        <Container maxWidth="xl">
-            <Typography variant="h5" component="h5"> 
-            <button className="usr-btn" onClick={()=>navigate('/')}>Back</button>
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [submitted,setSubmitted] = useState(false);
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      try {
+        setSubmitted(true);
+        const response = await Axios.post('http://localhost:3001/api/user/userlogin', {
+          email,
+          password,
+        });
+        console.log(response.data);
+        //console.log(response.data.status);
+        if (response.data.usersignup.status === 'success') {
+          // Store the token securely (e.g., in local storage or cookies)
+         // localStorage.setItem('authToken', response.data.token);
+          navigate('/empdashboard');
+        } else {
+          window.alert(response.data.message); // Display the error message
+        }
+  
+        setSubmitted(false);
+        setEmail("");
+        setPassword("");
+      } catch (error) {
+        console.error("Axios Error:", error);
+        setSubmitted(false); // Reset loading state after error
+        // Display a generic error message to the user
+        window.alert('An error occurred while logging in. Please try again.');
+      }
+    };
+  
+    return (
+      <Container maxWidth="xl" >
+          <navbar>
+      <Navbarr/>
+    </navbar>
+    <Table>
+     
+    <TableHead>
+        <h2>Login</h2>
+        </TableHead>
+  
+        <TableRow>
+          <TableCell>
             
-            </Typography>
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
-            <Typography variant="h2" component="h1">
-                Employee Login
-            </Typography>
-            <Typography variant="h5" sx={{ mt: 2 }}>
-              Login
-            </Typography>
-        </Box>
-        <Grid container spacing={4}>
-            <Table sx={{ mt: 4 }}>
-                <TableHead>
-
-
-                    <TableRow>
-
-
-                        <TableCell></TableCell>
-
-
-                        <TableCell></TableCell>
-
-
-                        <TableCell></TableCell>
-
-
-                    </TableRow>
-
-
-                </TableHead>
-
-
-                <TableBody>
-              {/*first row */}
-                        <TableRow >
-                            <TableCell></TableCell>
-                            <TableCell>
-                           Nic
-                            </TableCell>
-                            <TableCell>
-                            <input
-                        type="text"
-                        //value={textValue}
-                            //onChange={(event) => setTextValue(event.target.value)}
-    />
-                            </TableCell>
-                        </TableRow>
-            {/*second row */}
-                         <TableRow >
-                            <TableCell></TableCell>
-                            <TableCell>
-                      Password
-                            </TableCell>
-                            <TableCell>
-                            <input
-                        type="text"
-                        //value={textValue}
-                            //onChange={(event) => setTextValue(event.target.value)}
-    />
-                            </TableCell>
-                        </TableRow>
-             {/*third row */}
-             <TableRow >
-                            <TableCell></TableCell>
-                            <TableCell>
-                          
-                            </TableCell>
-                            <TableCell> <button className="usr-btn" onClick={()=>navigate('/empdashboard')}>Login</button></TableCell>
-                        </TableRow>
-                    
-                </TableBody>
-            </Table>
-        </Grid>
-    </Container>
-
-        
-
-
+        <label>Email address:</label>
+  
+          </TableCell>
+          <TableCell>
+          <input 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email} 
+        />
+  
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+   
+      
+          <label>Password:</label>
+          </TableCell>
+          <TableCell>
+          <input 
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
+        />
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+  
+          </TableCell>
+          <TableCell>
+          <Button disabled={submitted} 
+                  sx={{
+                    margin: "auto",
+                    marginBottom: "20px",
+                    backgroundColor: "black",  /* Set background color to black */
+                    color: "white",            /* Set text color to white */
+                    marginLeft: "15px",
+                    marginTop: "20px",
+                    "&:hover": {
+                      opacity: 0.7,
+                      backgroundColor: "black",  /* Maintain black background on hover */
+                    },
+                  }}
+          
+          onClick={handleSubmit}>
+          Login
+        </Button>
+          </TableCell>
+        </TableRow>
+   
+  
+       
+     
+  
+  
+  
+      </Table>
+      </Container>
     )
 }
 export default EmpLogin;
